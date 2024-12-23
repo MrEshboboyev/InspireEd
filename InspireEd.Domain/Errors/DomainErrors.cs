87e41fd98f669bc2f170b46d10eee32089c16054
mreshboboyev <mreshboboyev@gmail.com>
2024-12-23 14:08:27 +0500

    Create Faculty aggregate root entities : Faculty, Group;  Value objects : FacultyName, GroupName;  Repos : for that entities;  IRepository.cs is generic interface for aggregate root repos;

--- InspireEd.Domain/Errors/DomainErrors.cs
+++ InspireEd.Domain/Errors/DomainErrors.cs
@@ -7,7 +7,7 @@ namespace InspireEd.Domain.Errors;
 /// </summary>
 public static class DomainErrors
 {
-    #region User
+    #region Users
     
     #region Entities
     
@@ -62,6 +62,76 @@ public static class DomainErrors
         public static readonly Error TooLong = new(
             "LastName.TooLong",
             "Last name is too long");
+    }
+    
+    #endregion
+    
+    #endregion
+    
+    #region Faculties
+    
+    #region Entities
+    
+    public static class Faculty
+    {
+        public static readonly Error EmailAlreadyInUse = new(
+            "User.EmailAlreadyInUse",
+            "The specified email is already in use");
+
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            "Faculty.NotFound",
+            $"The faculty with the identifier {id} was not found.");
+
+        public static readonly Error NotExist = new(
+            "Faculties.NotExist",
+            $"There is no faculties");
+
+        public static readonly Error InvalidCredentials = new(
+            "User.InvalidCredentials",
+            "The provided credentials are invalid");
+    }
+    
+    public static class Group
+    {
+        public static readonly Error EmailAlreadyInUse = new(
+            "User.EmailAlreadyInUse",
+            "The specified email is already in use");
+
+        public static readonly Func<Guid, Error> NotFound = id => new Error(
+            "Group.NotFound",
+            $"The group with the identifier {id} was not found.");
+
+        public static readonly Error NotExist = new(
+            "Group.NotExist",
+            $"There is no groups");
+
+        public static readonly Error InvalidCredentials = new(
+            "User.InvalidCredentials",
+            "The provided credentials are invalid");
+    }
+
+    #endregion
+    
+    #region Value Objects
+
+    public static class FacultyName
+    {
+        public static readonly Error Empty = new(
+            "FacultyName.Empty",
+            "Faculty Name is empty");
+        public static readonly Error TooLong = new(
+            "FacultyName.TooLong",
+            "Faculty name name is too long");
+    }
+    
+    public static class GroupName
+    {
+        public static readonly Error Empty = new(
+            "GroupName.Empty",
+            "Group Name is empty");
+        public static readonly Error TooLong = new(
+            "GroupName.TooLong",
+            "Group name name is too long");
     }
     
     #endregion
---
+++ InspireEd.Domain/Faculties/Entities/Faculty.cs
@@ -0,0 +1,61 @@
+﻿using InspireEd.Domain.Faculties.ValueObjects;
+using InspireEd.Domain.Primitives;
+
+namespace InspireEd.Domain.Faculties.Entities;
+
+/// <summary>
+/// Represents a faculty entity.
+/// </summary>
+public sealed class Faculty : AggregateRoot, IAuditableEntity
+{
+    #region Private Fields
+
+    /// <summary>
+    /// A list of groups associated with the faculty.
+    /// </summary>
+    private readonly List<Group> _groups = [];
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Faculty"/> class with the specified ID and name.
+    /// </summary>
+    /// <param name="id">The unique identifier of the faculty.</param>
+    /// <param name="name">The name of the faculty.</param>
+    private Faculty(
+        Guid id,
+        FacultyName name) : base(id)
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Faculty"/> class.
+    /// </summary>
+    private Faculty()
+    {
+    }
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    /// Gets or sets the name of the faculty.
+    /// </summary>
+    public FacultyName Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the faculty was created in UTC.
+    /// </summary>
+    public DateTime CreatedOnUtc { get; set; }
+
+    /// <summary>
+    /// Gets or sets the date and time when the faculty was last modified in UTC.
+    /// </summary>
+    public DateTime? ModifiedOnUtc { get; set; }
+
+    #endregion
+}
