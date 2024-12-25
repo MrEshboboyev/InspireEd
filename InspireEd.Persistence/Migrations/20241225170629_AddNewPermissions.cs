1cbe591d3fd8fde88fe264ec318e614acbdf61b4
mreshboboyev <mreshboboyev@gmail.com>
2024-12-25 22:29:39 +0500

    Add Migration to new permissions

---
+++ InspireEd.Persistence/Migrations/20241225170629_AddNewPermissions.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace InspireEd.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 2, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 3, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 4, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 5, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "ViewProfile");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "ViewAttendance");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "ManageAttendance");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "ViewAssignedClasses");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "AddStudents");
+
+            migrationBuilder.InsertData(
+                table: "Permissions",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 6, "AssignGroups" },
+                    { 7, "AssignClasses" },
+                    { 8, "ViewFacultyData" },
+                    { 9, "FullAccess" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "RolePermission",
+                columns: new[] { "PermissionId", "RoleId" },
+                values: new object[,]
+                {
+                    { 5, 2 },
+                    { 3, 3 },
+                    { 4, 3 },
+                    { 1, 4 },
+                    { 2, 4 },
+                    { 9, 1 },
+                    { 6, 2 },
+                    { 7, 2 },
+                    { 8, 2 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 9, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 5, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 6, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 7, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 8, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 4, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "RolePermission",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 2, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "ReadUser");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "UpdateUser");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "AssignGroups");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "AssignClasses");
+
+            migrationBuilder.UpdateData(
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "ManageAttendance");
+
+            migrationBuilder.InsertData(
+                table: "RolePermission",
+                columns: new[] { "PermissionId", "RoleId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 1 },
+                    { 3, 2 },
+                    { 4, 2 },
+                    { 5, 3 }
+                });
+        }
+    }
+}
