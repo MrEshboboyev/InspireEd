2337c3b8267b22b3dbd12633606d8a6b0399c720
MrEshbobotev <mreshboboyev@gmail.com>
2025-02-17 09:45:43 -0800

    Set Up Arch unit Base test ;

--- tests/InspireEd.ArchitectureTests/BaseTest.cs
+++ tests/InspireEd.ArchitectureTests/BaseTest.cs
@@ -1,12 +1,12 @@
-﻿using InspireEd.Domain.Primitives;
-using System.Reflection;
+﻿using Assembly = System.Reflection.Assembly;
 
 namespace InspireEd.ArchitectureTests;
 
 public abstract class BaseTest
 {
     protected static readonly Assembly DomainAssembly = typeof(InspireEd.Domain.AssemblyReference).Assembly;
-    protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.AssemblyReference).Assembly;
     protected static readonly Assembly ApplicationAssembly = typeof(InspireEd.Application.AssemblyReference).Assembly;
+    protected static readonly Assembly PersistenceAssembly = typeof(Persistence.AssemblyReference).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(Infrastructure.AssemblyReference).Assembly;
     protected static readonly Assembly PresentationAssembly = typeof(Presentation.AssemblyReference).Assembly;
 }
